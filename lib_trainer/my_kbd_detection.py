3ef54da0e8cda6e9d3dd40430ecba5658e1871c5
Chuanwang WANG <chaunecy@outlook.com>
2020-08-21 15:46:21 +0800

    feat(load): update load function for allow and ign

--- lib_trainer/my_kbd_detection.py
+++ lib_trainer/my_kbd_detection.py
@@ -5,9 +5,9 @@ First, find all possible keyboard patterns,
 Then, detect all possible keyboard patterns in passwords.
 """
 import os
-from typing import Dict
+from typing import Dict, List, Tuple, Any
 
-from lib_trainer.my_dfs_lib import gen_dtree, extract, post_parse4case_free
+from .my_dfs_lib import gen_dtree, extract, post_parse4case_free
 
 path_kbd_allow = os.path.join(os.path.dirname(__file__), "kbd.allow")
 path_kbd_ignore = os.path.join(os.path.dirname(__file__), "kbd.ignore")
@@ -24,14 +24,14 @@ def kbd_filter(candidate: str):
     return True
 
 
-def load_kbd_allow() -> Dict[str, int]:
+def load_kbd_allow(file: str) -> Dict[str, int]:
     """
     words in this set will be treated as l33t and will not be parsed again
     :return: set of l33ts
     """
-    if not os.path.exists(path_kbd_allow):
+    if not os.path.exists(file):
         return {}
-    fd = open(path_kbd_allow, "r")
+    fd = open(file, "r")
     allow = {}
     for line in fd:
         allow[line.strip("\r\n")] = 1
@@ -39,32 +39,54 @@ def load_kbd_allow() -> Dict[str, int]:
     return allow
 
 
-def load_kbd_ign() -> Dict[str, int]:
+def load_kbd_ign(file: str) -> Dict[str, int]:
     """
     l33t.ignore, one instance per line
     :return: set of ignored l33ts
     """
-    if not os.path.exists(path_kbd_ignore):
+    if not os.path.exists(file):
         return {}
-    fd = open(path_kbd_ignore, "r")
+    fd = open(file, "r")
     ign = {}
     for line in fd:
         ign[line.strip("\r\n")] = 1
     return ign
 
 
-kbd_allow = load_kbd_allow()
-kbd_ign = load_kbd_ign()
-end = "\x03"
-kbd_dtree, max_kbd_len = gen_dtree(kbd_allow, end=end)
+def init(allow: str, ign: str, end: str = "\x03"):
+    kbd_allow = load_kbd_allow(allow)
+    kbd_ign = load_kbd_ign(ign)
+    kbd_dtree, max_kbd_len = gen_dtree(kbd_allow, end=end)
+    return kbd_dtree, max_kbd_len, kbd_allow, kbd_ign
 
 
-def kbd_detection(pwd: str):
+def kbd_detection(pwd: str, kbd_dtree: Dict, max_kbd_len: int, end: str = "\x03"):
     raw_kbd_lst = extract(dtree=kbd_dtree, pwd=pwd, max_kbd_len=max_kbd_len, end=end)
     sec_lst, kbd_lst = post_parse4case_free(raw_kbd_lst, pwd=pwd, tag="K")
     return sec_lst, kbd_lst
 
 
+def kbd_detection4seclist(sections: List[Tuple[str, Any]],
+                          kbd_dtree: Dict, max_kbd_len: int, end: str = "\x03"):
+    sec_list = []
+    kbd_list = []
+    for sec, tag in sections:
+        if tag is not None:
+            sec_list.append((sec, tag))
+        else:
+            sub_sec_list, sub_kbd_list = kbd_detection(sec, kbd_dtree, max_kbd_len, end)
+            sec_list.extend(sub_sec_list)
+            kbd_list.extend(sub_kbd_list)
+    return sec_list, kbd_list
+
+
+def test():
+    kbd_dtree, max_kbd_len, kbd_allow, kbd_ign = init(path_kbd_allow, path_kbd_ignore)
+    print(kbd_detection("1Q2w3e4rhh,", kbd_dtree, max_kbd_len))
+    pass
+
+
 if __name__ == '__main__':
-    print(kbd_detection("1Q2w3e4rhh"))
+    # print(kbd_detection("1Q2w3e4rhh"))
+    test()
     pass
