73df285f0e3a7ce70f15808cd1962b244fd3c818
Chuanwang WANG <chaunecy@outlook.com>
2020-11-19 18:48:39 +0800

    feat(scorer): update scorer

--- scorer.py
+++ scorer.py
@@ -7,20 +7,19 @@ from collections import Counter, defaultdict
 from math import log2, ceil
 from typing import List, Tuple, TextIO, Any, Dict
 
-import numpy
-from tqdm import tqdm
-
 from lib_scorer.grammar_io import load_grammar as load_grammar4scorer
 
 """
 Note that OMEN scorer is not considered here
 if prob for OMEN is not 0, use lib_scorer please
+WARNING: 
+L33t has not been considered. so passwords with l33t will not be correctly evaluated.
 """
 
 
 def extend_dict(counter: Counter):
     items = list(counter.keys())
-    cum_counts = numpy.array(list(counter.values())).cumsum()
+    cum_counts = my_cumsum(list(counter.values()))
     return counter, items, cum_counts
     pass
 
@@ -34,16 +33,27 @@ def pick_extend(extend: (Counter, List[str], [])):
     pass
 
 
-def gen_rank_from_minus_log_prob(minus_log_prob_pairs: List[Tuple[float, str]]) -> Tuple[numpy.ndarray, numpy.ndarray]:
+def my_cumsum(lst: List[float]):
+    if len(lst) <= 0:
+        return []
+    acc = 0
+    cumsum = []
+    for v in lst:
+        acc += v
+        cumsum.append(acc)
+    return cumsum
+
+
+def gen_rank_from_minus_log_prob(minus_log_prob_pairs: List[Tuple[float, str]]) -> Tuple[List[float], List[float]]:
     """
     calculate the ranks according to Monte Carlo method
     :param minus_log_prob_pairs: List of (minus log prob, password) tuples
     :return: minus_log_probs and corresponding ranks
     """
-    minus_log_probs = numpy.fromiter((lp for lp, _ in minus_log_prob_pairs), float)
+    minus_log_probs = [lp for lp, _ in minus_log_prob_pairs]
     minus_log_probs.sort()
     logn = log2(len(minus_log_probs))
-    positions = (2 ** (minus_log_probs - logn)).cumsum()
+    positions = my_cumsum([2 ** (mlp - logn) for mlp in minus_log_probs])
     return minus_log_probs, positions
     pass
 
@@ -52,74 +62,6 @@ def minus_log_prob2rank(minus_log_probs, positions, minus_log_prob):
     idx = bisect.bisect_right(minus_log_probs, minus_log_prob)
     return positions[idx - 1] if idx > 0 else 1
     pass
-
-
-lds_re = re.compile(r"([a-zA-Z]+|[0-9]+|[\x21-\x2f\x3a-\x40\x5b-\x60\x7b-\x7e]+)")
-terminal_re = re.compile(r"([ADKOXY]\d+)")
-
-
-def extract_lds(pwd: str) -> str:
-    segs = [s for s in lds_re.split(pwd) if len(s) > 0]
-    ret = ""
-    for seg in segs:
-        if seg.isalpha():
-            ret += ("L" * len(seg))
-        elif seg.isdigit():
-            ret += ("D" * len(seg))
-        else:
-            ret += ("S" * len(seg))
-        pass
-    return ret
-    pass
-
-
-def aod2lds(raw_struct: str) -> (str, List[Tuple[int, int]], int):
-    # if raw_struct.find("X") > -1 or raw_struct.find("K") > -1:
-    #     return use_all
-    parts = terminal_re.findall(raw_struct)
-    res = ""
-    plen = 0
-    rm = []
-    start_pos = 0
-    for p in parts:
-        tag, span = p[0], int(p[1:])
-        add_len = span
-        if tag == 'A':
-            res += ("L" * span)
-        elif tag == 'D':
-            res += ("D" * span)
-        elif tag == 'O':
-            res += ("S" * span)
-        elif tag == 'Y':
-            res += "DDDD"
-            add_len *= 4
-        elif tag == 'K':
-            rm.append((start_pos, span))
-        elif tag == 'X':
-            add_len *= 2
-            rm.append((start_pos, add_len))
-            pass
-        start_pos += add_len
-        plen += add_len
-    # if len(res) == plen:
-    return res, rm, plen
-    # else:
-    #     print(f"struct for {raw_struct} does't match in length: {res}", file=sys.stderr)
-    #     sys.exit(-1)
-    pass
-
-
-def rm_substr(struct: str, rm: List[Tuple[int, int]]) -> str:
-    res = ""
-    for i, s in enumerate(struct):
-        need_rm = False
-        for start, span in rm:
-            if start <= i < start + span:
-                need_rm = True
-                break
-        if not need_rm:
-            res += s
-    return res
 
 
 class MyScorer:
@@ -154,31 +96,31 @@ class MyScorer:
         self.__load_grammars()
         self.__terminal_re = re.compile(r"([ADKOXY]\d+)")
         print("Done!", file=sys.stderr)
+        base_struct_tree = {}
+        for base_struct in self.count_base_structures:
+            terminals = self.__terminal_re.findall(base_struct)
+            tree = base_struct_tree
+            for terminal in terminals:
+                tag = terminal[0]
+                num = int(terminal[1:])
+                n_terminal = (tag, num)
+                if n_terminal not in tree:
+                    tree[n_terminal] = {}
+                tree = tree[n_terminal]
+            tree[("END", 0)] = self.count_base_structures.get(base_struct)
+        self.base_struct_tree = base_struct_tree
+        all_terminals = {}
+        for counter, tag in [(self.count_alpha, "A"), (self.count_alpha_masks, "C"), (self.count_digits, "D"),
+                             (self.count_other, "O"), (self.count_keyboard, "K")]:
+            for _len, items in counter.items():
+                all_terminals[(tag, _len)] = items
 
-        luds2base_structures = {}
-        filtered = defaultdict(lambda: [])
-
-        for struct in tqdm(self.count_base_structures, desc="Pre-processing, stage 1: "):
-            lds, rm, plen = aod2lds(struct)
-            if len(rm) != 0:
-                filtered[plen].append((lds, rm, struct))
-                pass
-            else:
-                if lds not in luds2base_structures:
-                    luds2base_structures[lds] = set()
-                luds2base_structures[lds].add(struct)
             pass
-        for s in tqdm(luds2base_structures, desc="Pre-processing, stage 2: "):
-            ls = len(s)
-            if ls not in filtered:
-                continue
-            add_ons = filtered.get(ls)
-            for lds, rm, origin_struct in add_ons:
-                rmd = rm_substr(s, rm)
-                if rmd == lds:
-                    luds2base_structures[s].add(origin_struct)
-        del filtered
-        self.lds2base_structures = luds2base_structures
+        for items, tag in [(self.count_context_sensitive, "X"), (self.count_years, "Y")]:
+            all_terminals[(tag, 1)] = items
+            pass
+        self.all_terminals = all_terminals
+
         self.__extend_structure = extend_dict(self.count_base_structures)
         self.__extend_years = extend_dict(self.count_years)
         self.__extend_context = extend_dict(self.count_context_sensitive)
@@ -192,72 +134,74 @@ class MyScorer:
         load_grammar4scorer(self, rule_directory=self.rule)
         pass
 
-    def calc_prob(self, pwd: str) -> float:
-        # lpwd = len(pwd)
-        struct = extract_lds(pwd)
-        try:
-            structs = self.lds2base_structures[struct]
-        except KeyError:
-            return 0
+    def my_calc_prob(self, pwd: str, cap: str, idx: int, root: Dict, acc_prob: float, max_prob: List[float]):
+        if idx == len(pwd):
+            if ("END", 0) in root:
+                final_prob = root[("END", 0)] * acc_prob
+                if final_prob > max_prob[0]:
+                    max_prob[0] = final_prob
+            return
+        for terminal in root:
+            tag, _len = terminal
 
-        prob_list = []
-        for s in structs:
-            prob = 1.0
-            prob *= self.count_base_structures.get(s, 0.0)
-            terminals = self.__terminal_re.findall(s)
-            start_pos = 0
-            for t in terminals:
-                tag, span = t[0], int(t[1:])
-                addon = span
-                if tag == 'Y':
-                    addon *= 4
-                elif tag == 'X':
-                    addon *= 2
-                pwd_part = pwd[start_pos:start_pos + addon]
-                if tag == 'A':
-                    prob *= self.count_alpha.get(len(pwd_part), {}).get(pwd_part.lower(), 0.0)
-                    if prob <= self.minimal_prob:
-                        break
-                    alpha_mask = ''
-                    for p in pwd_part:
-                        if p.isupper():
-                            alpha_mask += 'U'
-                        else:
-                            alpha_mask += "L"
-                    prob *= self.count_alpha_masks.get(len(alpha_mask), {}).get(alpha_mask, 0.0)
-                elif tag == 'O':
-                    prob *= self.count_other.get(len(pwd_part), {}).get(pwd_part, 0.0)
-                elif tag == 'D':
-                    prob *= self.count_digits.get(len(pwd_part), {}).get(pwd_part, 0.0)
-                elif tag == 'K':
-                    prob *= self.count_keyboard.get(len(pwd_part), {}).get(pwd_part, 0.0)
-                elif tag == 'X':
-                    prob *= self.count_context_sensitive.get(pwd_part, 0.0)
-                elif tag == 'Y':
-                    prob *= self.count_years.get(pwd_part, 0.0)
-                else:
-                    print(f"unknown tag: {tag} in {s} for {pwd}")
-                    sys.exit(-1)
-                    pass
-                start_pos += addon
-                if prob == 0:
-                    break
+            if tag == "A":
+                end_idx = idx + _len
+                if end_idx > len(pwd):
+                    continue
+                part = pwd[idx: end_idx]
+                mask = cap[idx: end_idx]
+                part_prob = self.count_alpha.get(_len, {}).get(part, .0)
+                mask_prob = self.count_alpha_masks.get(_len, {}).get(mask, .0)
+                mul_prob = part_prob * mask_prob
+                # if mul_prob < sys.float_info.min:
+                #     continue
+            elif tag == 'Y':
+                end_idx = idx + 4
+                if end_idx > len(pwd):
+                    continue
+                part = pwd[idx: end_idx]
+                mul_prob = self.count_years.get(part, .0)
+            elif tag == 'X':
+                for addon in [2, 3, 4]:
+                    if idx + addon > len(pwd):
+                        continue
+                    part = pwd[idx:idx + addon]
+                    mul_prob = self.count_context_sensitive.get(part, .0)
+
+                    if mul_prob > sys.float_info.min:
+                        self.my_calc_prob(pwd, cap, idx + addon, root[terminal], acc_prob * mul_prob, max_prob)
+                continue
+            elif tag != "END":
+                # D, O, K
+                end_idx = idx + _len
+                if end_idx > len(pwd):
+                    continue
+                part = pwd[idx: end_idx]
+                mul_prob = self.all_terminals[terminal].get(part, .0)
                 pass
-            if prob != 0:
-                prob_list.append(prob)
+            else:
+                continue
+            if mul_prob > sys.float_info.min:
+                tmp_acc_prob = acc_prob * mul_prob
+                self.my_calc_prob(pwd, cap, end_idx, root[terminal], tmp_acc_prob, max_prob)
             pass
-        if len(prob_list) == 0:
-            return 0
-        else:
-            return max(prob_list)
+        pass
 
     def minus_log2_prob(self, pwd: str) -> float:
-        prob = self.calc_prob(pwd)
-        return -log2(max(prob, self.minimal_prob))
+        # prob = self.calc_prob(pwd)
+        cap = ""
+        for c in pwd:
+            if c.isupper():
+                cap += "U"
+            else:
+                cap += "L"
+        max_prob = [sys.float_info.min]
+        self.my_calc_prob(pwd.lower(), cap, 0, self.base_struct_tree, 1.0, max_prob)
+        return -log2(max(max_prob[0], self.minimal_prob))
 
     def calc_minus_log2_prob_from_file(self, passwords: TextIO) -> Dict[Any, Tuple[int, float]]:
         """
-
+        return a dict, whose key is pwd, value is tuple of (appearance, minus_log_prob)
         :param passwords: passwords, do not close it please!
         :return:
         """
@@ -267,20 +211,28 @@ class MyScorer:
             pwd = pwd.strip("\r\n")
             raw_pwd_counter[pwd] += 1
         pwd_counter = defaultdict(lambda: (0, .0))
-        for pwd, num in tqdm(iterable=raw_pwd_counter.items(), total=len(raw_pwd_counter),
-                             desc="Calc prob: "):
+        total_items = len(raw_pwd_counter)
+        print("Calc prob...", file=sys.stderr)
+        i = 0
+        back = "\b" * 21
+        for pwd, num in raw_pwd_counter.items():
             pwd_counter[pwd] = (num, self.minus_log2_prob(pwd))
+            i += 1
+            print(f"{i:10d}/{total_items:10d}{back}", end="", file=sys.stderr)
         return pwd_counter
         pass
 
-    def gen_n_rand_pwd(self, n: int = 10000) -> List[Tuple[float, str]]:
+    def gen_n_rand_pwd(self, n: int) -> List[Tuple[float, str]]:
         pairs = []
-        for _ in tqdm(iterable=range(n), desc="Sampling: "):
+        for _ in range(n):
             pairs.append(self.gen_rand_pwd())
         return pairs
-        pass
 
     def gen_rand_pwd(self) -> Tuple[float, str]:
+        """
+        generate a random password and get its minus log probability
+        :return: (minus_log_prob, pwd)
+        """
         log_prob = 0
         pwd = ""
         ext_structs = self.__extend_structure
@@ -350,18 +302,23 @@ def monte_carlo_wrapper(rule: str, target: TextIO, save2: TextIO, n: int = 10000
     print(f"rule: {rule}", file=sys.stderr)
     print(f"target: {target.name}", file=sys.stderr)
     pcfg_scorer = MyScorer(rule=rule)
+    # sampling n passwords
     rand_pairs = pcfg_scorer.gen_n_rand_pwd(n=n)
+    # generate corresponding rank list
     minus_log_prob_list, ranks = gen_rank_from_minus_log_prob(rand_pairs)
     del rand_pairs
+    # scoring passwords in test set
     scored_pwd_list = pcfg_scorer.calc_minus_log2_prob_from_file(passwords=target)
     target.close()
     del pcfg_scorer
     cracked = 0
     prev_rank = 0
     total = sum([n for n, _ in scored_pwd_list.values()])
-    for pwd, info in tqdm(iterable=sorted(scored_pwd_list.items(), key=lambda x: x[1][1], reverse=False),
-                          total=len(scored_pwd_list), desc="Estimating: "):
+    # estimating
+    print("Estimating...", file=sys.stderr)
+    for pwd, info in sorted(scored_pwd_list.items(), key=lambda x: x[1][1], reverse=False):
         num, mlp = info
+        # rank should be an integer, and larger than previous one
         rank = ceil(max(minus_log_prob2rank(minus_log_prob_list, ranks, mlp), prev_rank + 1))
         prev_rank = rank
         cracked += num
@@ -392,16 +349,11 @@ def main():
 
 
 def test():
-    pcfg_scorer = MyScorer(rule="./Rules/Origin/rockyou")
+    pcfg_scorer = MyScorer(rule="/home/cw/Documents/Experiments/SegLab/Rules/DRed/rockyou")
     usr_in = ""
     while usr_in != "exit":
         usr_in = input("Type in password: ")
         print(pcfg_scorer.minus_log2_prob(usr_in))
-    # print(pcfg_scorer.minus_log2_prob("iluvyandel"))
-    # print(pcfg_scorer.minus_log2_prob("0O9I8U7Y"))
-    # print(pcfg_scorer.minus_log2_prob("custom"))
-    # print(pcfg_scorer.minus_log2_prob("imsocool"))
-    pass
 
 
 if __name__ == '__main__':
